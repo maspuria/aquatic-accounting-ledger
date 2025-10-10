09d010073fcc8f9ba6338f38783497d20ff1e600
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-10 16:01:38 -0500

    constructors and getters commit

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -8,4 +8,34 @@ public class Transaction {
     private String vendor;
     private double amount;
 
+    // Constructors
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    //Getters
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
 }
