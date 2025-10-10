10d6da3ae96527c4ec2cb06b669b3381b3747118
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-10 13:39:23 -0500

    add scanner in application commit

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,7 +1,11 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hi! Capstone Week! Woohoo!");
+        Scanner scanner = new Scanner(System.in);
+
     }
 }
