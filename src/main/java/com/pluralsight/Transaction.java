d592cbaa753f2366680f9b6bf69fb1288e85a1e2
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-10 13:49:20 -0500

    field members commit

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,5 +1,11 @@
 package com.pluralsight;
 
 public class Transaction {
+    // Field Members
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
 
 }
