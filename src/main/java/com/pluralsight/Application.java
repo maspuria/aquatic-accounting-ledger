9a545f72d315725a5ad93485eabc70fa62115971
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-13 14:35:36 -0500

    ledger menu screen outline commit

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class Application {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -21,7 +21,7 @@ public class Application {
                     System.out.println("Make Payment (Deposit)");
                     break;
                 case "L":
-                    System.out.println("Ledger Menu");
+                    runLedgerMenu();
                     break;
                 case "X":
                     System.out.println("You selected Exit. Thank you for using Aquatic Accounting Ledger... Goodbye!");
@@ -35,7 +35,7 @@ public class Application {
         scanner.close();
 
     }
-
+    // I made my Home Screen Menu into a method so that my main method can be clean and organized
     public static void displayHomeScreenMenu() {
         System.out.println("==== Home Screen Menu ====");
         System.out.println("(D) Add Deposit ");
@@ -44,5 +44,42 @@ public class Application {
         System.out.println("(X) Exit ");
         System.out.print("Enter choice: ");
     }
+
+    // Creating the Ledger Menu
+    public static void runLedgerMenu() {
+        boolean running = true;
+        while (running) {
+            System.out.println("==== Ledger Menu ====");
+            System.out.println("(A) Display All Entries ");
+            System.out.println("(D) Display Deposits "); // only entries that deposits into the account
+            System.out.println("(P) Display Payments "); // only negative entries (payments)
+            System.out.println("(R) Reports "); // another menu screen that allows custom searches
+            System.out.println("(H) Home"); // takes you back to the original home screen menu
+            System.out.print("Enter choice: ");
+
+            String choice = scanner.nextLine().trim().toUpperCase();
+
+            switch (choice) {
+                case "A":
+                    System.out.println("Displaying All Entries");
+                    break;
+                case"D":
+                    System.out.println("Displaying Deposits only");
+                    break;
+                case"P":
+                    System.out.println("Displaying Payments");
+                    break;
+                case"R":
+                    System.out.println("Displaying Reports Menu now");
+                    break;
+                case"H":
+                    running = false; // this will exit the ledger menu and take you back to the home screen menu
+                    break;
+                default:
+                    System.out.println("Invalid entry choice. Enter A, D, P, R, or H. Please try again.");
+            }
+        }
+    }
+
 
 }
